40bdc21bda7ba457e5c08dcc1dc59ec694e5cf50
Hollis Akins <hollis.akins@gmail.com>
2018-09-27 15:32:39 -0500

    updates

--- footer.js
+++ footer.js
@@ -1,4 +1,2 @@
-document.write("<div class='background-footer'></div>");
-document.write("<div class='footer'>&copy; 2018 Hollis Akins</div>")
 document.write("</body>");
 document.write("</html>");
