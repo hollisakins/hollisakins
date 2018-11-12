73202417c6679c680e36345b2b5f51f0882bdf8d
Hollis Akins <hollis.akins@gmail.com>
2018-11-12 14:48:45 -0600

    listing page

--- header.js
+++ header.js
@@ -11,24 +11,43 @@ document.write("<h1>");
 document.write("<a href='index.html'>hollis akins</a>");
 document.write("</h1>");
 document.write("<div class='menu'>");
+
+document.write("<div class='dropdown'>");
+document.write("<a href='mailto:hollis.akins@gmail.com'><button class='dropbtn'>contact</button></a>");
+document.write("<div class='dropdown-content'>");
+document.write("</div></div>");
+
 document.write("<div class='dropdown'>");
 document.write("<a href='about.html'><button class='dropbtn'>about</button></a>");
 document.write("<div class='dropdown-content'>");
 document.write("</div></div>");
 
 document.write("<div class='dropdown'>");
-document.write("<button class='dropbtn'>projects</button>");
+document.write("<a href='photography.html'><button class='dropbtn'>photo</button></a>");
 document.write("<div class='dropdown-content'>");
-    document.write("<div class='dropdown2'>");
-    document.write("<a href='research.html'><button class='dropbtn2'>research</button></a>");
-    document.write("</div>");
-    document.write("<div class='dropdown2'>");
-    document.write("<a href='photography.html'><button class='dropbtn2'>photography</button></a>");
-    document.write("</div>");
-    document.write("<div class='dropdown2'>");
-    document.write("<a href='misc.html'><button class='dropbtn2'>miscellaneous</button></a>");
-    document.write("</div>");
 document.write("</div></div>");
+
+
+document.write("<div class='dropdown'>");
+document.write("<a href='projects.html'><button class='dropbtn'>projects</button></a>");
+document.write("<div class='dropdown-content'>");
+document.write("</div></div>");
+
+
+
+// document.write("<div class='dropdown'>");
+// document.write("<button class='dropbtn'>projects</button>");
+// document.write("<div class='dropdown-content'>");
+//     document.write("<div class='dropdown2'>");
+//     document.write("<a href='research.html'><button class='dropbtn2'>research</button></a>");
+//     document.write("</div>");
+//     document.write("<div class='dropdown2'>");
+//     document.write("<a href='photography.html'><button class='dropbtn2'>photography</button></a>");
+//     document.write("</div>");
+//     document.write("<div class='dropdown2'>");
+//     document.write("<a href='misc.html'><button class='dropbtn2'>miscellaneous</button></a>");
+//     document.write("</div>");
+// document.write("</div></div>");
 
 document.write("</div>");
 document.write("</nav>");
