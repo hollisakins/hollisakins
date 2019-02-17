361da1e04fe18c825a291c484314bfed52f240ec
Hollis Akins <hollis.akins@gmail.com>
2019-02-17 14:29:07 -0600

    layout test

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@
 document.write("<div style='width:100%;height:50px;'></div>")
 document.write("<div class='background-footer'>")
 document.write("<div class='footer'>")
-document.write("All work &copy; 2018 Hollis Akins")
+document.write("All work &copy; 2019 Hollis Akins")
 document.write("</div>")
 document.write("</div>")
 
---
+++ layout/subheader.js
@@ -0,0 +1,62 @@
+document.write("<!DOCTYPE html>");
+document.write("<html>");
+document.write("<head>");
+document.write("<title>Hollis Akins</title>");
+document.write("<meta charset='UTF-8'>");
+document.write("<link rel='stylesheet' href='style.css'>");
+document.write("<script async src='https://www.googletagmanager.com/gtag/js?id=UA-57804746-2'></script>")
+document.write("<script>")
+document.write("window.dataLayer = window.dataLayer || [];")
+document.write("function gtag(){dataLayer.push(arguments);}")
+document.write("gtag('js', new Date());")
+document.write("gtag('config', 'UA-57804746-2');")
+document.write("</script>")
+
+
+document.write("</head><body>");
+document.write("<div class='background-header'></div>")
+document.write("<nav>");
+document.write("<h1>");
+document.write("<a href='../index.html'>hollis akins</a>");
+document.write("</h1>");
+document.write("<div class='menu'>");
+
+document.write("<div class='dropdown'>");
+document.write("<a href='mailto:hollis.akins@gmail.com'><button class='dropbtn'>contact</button></a>");
+document.write("<div class='dropdown-content'>");
+document.write("</div></div>");
+
+document.write("<div class='dropdown'>");
+document.write("<a href='../index.html'><button class='dropbtn'>about</button></a>");
+document.write("<div class='dropdown-content'>");
+document.write("</div></div>");
+
+document.write("<div class='dropdown'>");
+document.write("<a href='../photography.html'><button class='dropbtn'>photo</button></a>");
+document.write("<div class='dropdown-content'>");
+document.write("</div></div>");
+
+
+document.write("<div class='dropdown'>");
+document.write("<a href='../projects.html'><button class='dropbtn'>projects</button></a>");
+document.write("<div class='dropdown-content'>");
+document.write("</div></div>");
+
+
+
+// document.write("<div class='dropdown'>");
+// document.write("<button class='dropbtn'>projects</button>");
+// document.write("<div class='dropdown-content'>");
+//     document.write("<div class='dropdown2'>");
+//     document.write("<a href='research.html'><button class='dropbtn2'>research</button></a>");
+//     document.write("</div>");
+//     document.write("<div class='dropdown2'>");
+//     document.write("<a href='photography.html'><button class='dropbtn2'>photography</button></a>");
+//     document.write("</div>");
+//     document.write("<div class='dropdown2'>");
+//     document.write("<a href='misc.html'><button class='dropbtn2'>miscellaneous</button></a>");
+//     document.write("</div>");
+// document.write("</div></div>");
+
+document.write("</div>");
+document.write("</nav>");
